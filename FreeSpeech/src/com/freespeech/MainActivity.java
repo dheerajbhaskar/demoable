98b92c2a97f55f4da84dd941edcd403824fab547
Dheeraj Bhaskar <dheerajthe1@gmail.com>
2013-02-02 23:35:10 +0530

    commented out creation of dummyFragments in getItem

--- FreeSpeech/src/com/freespeech/MainActivity.java
+++ FreeSpeech/src/com/freespeech/MainActivity.java
@@ -69,10 +69,11 @@ public class MainActivity extends FragmentActivity {
 			// getItem is called to instantiate the fragment for the given page.
 			// Return a DummySectionFragment (defined as a static inner class
 			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
-			Bundle args = new Bundle();
-			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-			fragment.setArguments(args);
+			// Fragment fragment = new DummySectionFragment();
+			// Bundle args = new Bundle();
+			// args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position +
+			// 1);
+			// fragment.setArguments(args);
 
 			switch (position) {
 			case CONSTANTS.FRAGMENT_INDEX_CHAT:
